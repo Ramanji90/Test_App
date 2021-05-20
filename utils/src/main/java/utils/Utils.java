1ff0a79fafdfb3f7a525884a345740b0033aed0e
Ramanji90 <ramanjaneyappa@gmail.com>
2021-05-20 22:35:06 +0530

    first

---
+++ utils/src/main/java/utils/Utils.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class Utils {
+
+	public static String encoder(String pass)
+	{
+		//Encoder Logic
+       Encoder encoder = Base64.getEncoder();
+       String encodedPass = encoder.encodeToString(pass.getBytes());
+       return encodedPass;
+	}
+	
+	public static byte[] decoder(String encodedpass)
+	{
+       Decoder decoder = Base64.getDecoder();
+       byte[] decodedPass = decoder.decode(encodedpass.getBytes());
+       return decodedPass;
+	}
+
+}
